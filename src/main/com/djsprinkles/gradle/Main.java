a44e990594722edcc4d6478df5d7b668097a35dc
randomsprinkles <lilleofred02@gmail.com>
2017-11-22 21:35:27 -0500

    Update Main.java

--- src/main/com/djsprinkles/gradle/Main.java
+++ src/main/com/djsprinkles/gradle/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		NetworkTable.setClientMode();
-		NetworkTable.setTeam(4525);
+		NetworkTable.setTeam(TEAM);
 		NetworkTable.initialize();
 		NetworkTable table = NetworkTable.getTable("Test");
 		
